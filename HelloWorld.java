9d37f74ce4db226bbd1bebf54988ddd44553df42
Saidulu Alli <saidulu.alli@gmail.com>
2017-02-20 08:14:10 -0800

    push demo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWrold{
 public static void main(String args[]){
-System.out.println("Welcome to Github);
+System.out.println("Welcome to Github");
 System.out.println("Pull demo");
+System.out.println("Push Demo");
 }
 }
